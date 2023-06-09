b4787c4c162eaa879841acd415e200e98e63b98c
MilinMiroliya <milinmiroliya2368@gmail.com>
2023-06-09 22:02:47 +0530

    MM

--- lib/main.dart
+++ lib/main.dart
@@ -6,37 +6,56 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("Sky Scrapper"),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Lottie.asset("assets/animations/weather_anim.json"),
-        ],
+      backgroundColor: const Color(0xff2f2f85),
+      // appBar: AppBar(
+      //   title: const Text("Sky Scrapper"),
+      // ),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  height: 230,
+                  width: 260,
+                  decoration: BoxDecoration(boxShadow: [
+                    BoxShadow(
+                      color: Colors.white.withOpacity(0.62),
+                      spreadRadius: 5,
+                      blurRadius: 10000,
+                    ),
+                  ]),
+                ),
+                Lottie.asset("assets/animations/weather_anim.json"),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
