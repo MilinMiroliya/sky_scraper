f011696f5eb1c45d7b5d7a236945a1bb2d1986ed
dhruvin2004 <dhruvinpandav999@gmail.com>
2023-06-10 08:35:05 +0530

    MM

--- lib/main.dart
+++ lib/main.dart
@@ -43,15 +43,78 @@ class _MyHomePageState extends State<MyHomePage> {
                 Container(
                   height: 230,
                   width: 260,
-                  decoration: BoxDecoration(boxShadow: [
-                    BoxShadow(
-                      color: Colors.white.withOpacity(0.62),
-                      spreadRadius: 5,
-                      blurRadius: 10000,
-                    ),
-                  ]),
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.white.withOpacity(0.62),
+                        spreadRadius: 5,
+                        blurRadius: 10000,
+                      ),
+                    ],
+                  ),
                 ),
                 Lottie.asset("assets/animations/weather_anim.json"),
+              ],
+            ),
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(
+                          left: 40, right: 40, top: 40, bottom: 6),
+                      child: Text(
+                        "Weather",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.amber,
+                          fontSize: 32,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 40),
+                      child: Text(
+                        "Forecast App.",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                          fontSize: 32,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 40),
+                      child: Text(
+                        "It's the newest weather app. It has a bunch of features and that includes most of the ones that every weather app has.",
+                        style: TextStyle(
+                          fontSize: 13,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  height: 80,
+                  width: 200,
+                  decoration: BoxDecoration(),
+                ),
               ],
             ),
           ],
